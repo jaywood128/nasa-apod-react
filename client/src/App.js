9167f70061c84dd590a64205750937f31ed35d69
Johnathon Wood <jaywood128@gmail.com>
2019-08-29 18:44:48 -0400

    App.js is not mounting. Moved index.js into reducers directory -- App is no longer mounting.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { 
-  HashRouter as Router, 
-  Route
-} from 'react-router-dom';
 import AstronomyPicturesContainer from './components/AstronomyPicturesContainer';
 
 
 function App() {
   return (
     <div className="App">
-    <Router>
-        <Route exact path="/" component={AstronomyPicturesContainer} />
-        {/* <Route exact path="/api/v1/astronomy_pictures" component={AstronomyPictureSearch} /> */}
-       {/* <AstronomyPicturesContainer />  */}
-    </Router> 
+      <h2> Inside App.js</h2>
+       <AstronomyPicturesContainer /> 
+    
     </div>
   );
 }
---
+++ client/src/reducers/commentsReducer.js
@@ -0,0 +1,13 @@
+export default function commentsReducer (state = {
+  pictures: []
+}, action) {
+  switch(action.type) {
+    case 'ADD_COMMENT' : 
+      
+  default: 
+    return state;
+  }
+
+}
+
+
