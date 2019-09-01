e2465046304c0bed960ba20476dec3ceb85ba914
Johnathon Wood <jaywood128@gmail.com>
2019-09-01 18:11:37 -0400

    Create rake task for fetching

--- Rakefile
+++ Rakefile
@@ -4,3 +4,5 @@
 require_relative 'config/application'
 
 Rails.application.load_tasks
+
+Dir.glob(File.join('lib/tasks/**/*.rake')).each { |file| load file }
--- client/src/components/AstronomyPictures.jsx
+++ client/src/components/AstronomyPictures.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const AstronomyPictures = ({ pictures }) => (pictures.map(Picture))
+const AstronomyPictures = ({ pictures }) => (pictures.map((Picture)))
 
 const Picture = ({id, date, explanation, hdurl, media_type, title, url}) => {
   return (
-  <div className="picture">
+  <div key ={id} className="picture">
     <h3> id: {id} </h3> 
     <h3> date: {date} </h3> 
     <p> Explanation: {explanation} </p> 
-    <h3> hdurl: {hdurl}</h3>
+    <a href ={hdurl}> </a>
     <h3> media_type: {media_type} </h3>
     <h1> title: {title} </h1> 
     <img src={ url } />
