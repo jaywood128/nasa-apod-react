afe26453001ceade81d40306f098d567fae7e407
Johnathon Wood <jaywood128@gmail.com>
2019-09-14 19:28:15 -0400

    Invalid hook call; trying to use react-bootstrap.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,30 @@ import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AstronomyPictureContainer from './components/AstronomyPictureContainer';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import AstronomyPicturesLastThirtyDaysContainer from './components/AstronomyPicturesLastThirtyDaysContainer'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layouts';
+import { NavigationBar } from './components/NavigationBar';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <AstronomyPictureContainer /> 
+        <React.Fragment/> 
+          <Layout/> 
+            <Router>
+              <NavigationBar/>
+              <Switch> 
+                <Route exact path='/' component={AstronomyPictureContainer }/> 
+                  <Route path='/last_thirty_days' component={AstronomyPicturesLastThirtyDaysContainer}/> 
+                  {/* <Route component={NoMatch}/>  */}
+              </Switch>
+            </Router>
+        <React.Fragment /> 
       </div>
     );
   }
