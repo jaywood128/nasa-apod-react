e474e399d5856d3fc48027fe4003a4470ce72eea
Johnathon Wood <jaywood128@gmail.com>
2019-10-25 16:24:05 -0400

    Commented out AstronomyPictureSerializer attributes

--- app/serializers/astronomy_pictures_serializer.rb
+++ app/serializers/astronomy_pictures_serializer.rb
@@ -1,5 +1,5 @@
 class AstronomyPicturesSerializer < ActiveModel::Serializer
   attributes :id
   has_many :user_astronomy_pictures
-  attributes: :user_id, :astronomy_picture_id, :is_favorite
+  # attributes: :user_id, :astronomy_picture_id, :is_favorite
 end
