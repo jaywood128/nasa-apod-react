6296a3850f3e0860b3deba0724dc518cd77f8b1f
Johnathon Wood <jaywood128@gmail.com>
2019-10-25 16:13:42 -0400

    Commented out rake controller

--- app/controllers/rake_controller.rb
+++ app/controllers/rake_controller.rb
@@ -1,13 +1,13 @@
 
-require 'rake'
+# require 'rake'
 
-Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
-Sample::Application.load_tasks 
+# Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
+# Sample::Application.load_tasks 
 
-class RakeController < ApplicationController
-  def run
-    Rake::Task[params[:last_thirty_days]].reenable # in case you're going to invoke the same task second time.
-    Rake::Task[params[:last_thirty_days]].invoke
-  end
+# class RakeController < ApplicationController
+#   def run
+#     Rake::Task[params[:last_thirty_days]].reenable # in case you're going to invoke the same task second time.
+#     Rake::Task[params[:last_thirty_days]].invoke
+#   end
 
-end
+# end
