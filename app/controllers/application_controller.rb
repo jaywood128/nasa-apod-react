c1b4eead9907199b7fb3cbba238bfe3750609c69
Johnathon Wood <jaywood128@gmail.com>
2019-12-22 20:46:37 -0500

    Fixing welcome controler and sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery prepend: true
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true
   before_action :authenticate_user!, only: [:app]
 
   def after_sign_in_path_for(resource)
     app_path 
   end
 
-  # def after_sign_up_path_for(resource)
-  #   app_path 
-  # end
+  def after_sign_up_path_for(resource)
+    app_path 
+  end
 
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.sign_out_via = :get
+  config.router_name = :main_app
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
