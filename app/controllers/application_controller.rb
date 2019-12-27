322b7af91c30ae029ff34f90e3c91f545e69a631
Johnathon Wood <jaywood128@gmail.com>
2019-12-27 13:59:54 -0500

    Commented out skip_before_action and changed i18n fall back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   # before_action :authenticate_user!, only: [:app]
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
 
 
