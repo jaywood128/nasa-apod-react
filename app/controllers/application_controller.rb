426dcdcb838c0389c02b178da7ff539aa1f0a7ab
Johnathon Wood <jaywood128@gmail.com>
2020-01-01 09:34:05 -0500

    Adding background-image for views/welcome/home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   # before_action :authenticate_user!, only: [:app]
   # skip_before_action :verify_authenticity_token
-
-
+  def after_sign_in_path_for(resource)
+    '/app'
+  end 
+  
+  def after_sign_up_path_for(resource)
+    '/app'
+  end
 
 end
