705df2547167625e3c77d5eddaeeeddf2b9c3282
Johnathon Wood <jaywood128@gmail.com>
2020-01-02 18:38:38 -0500

    removed cdn from app/views/layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 class SessionsController < Devise::SessionsController 
   skip_before_action :verify_authenticity_token, :only => :create
   
-  def after_sign_in_path_for(resource)
-    '/app'
-  end 
+ 
 
   
 end 
