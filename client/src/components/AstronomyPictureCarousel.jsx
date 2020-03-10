e90cf8dfa8f3d7c432670dfbc38128a250f2249d
Johnathon Wood <jaywood128@gmail.com>
2020-03-10 19:34:31 -0400

    Added background image to landing page

--- client/src/components/AstronomyPictureCarousel.jsx
+++ client/src/components/AstronomyPictureCarousel.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
  const AstronomyPictureCarousel = (props) => { 
